3c88d7578e139e784cda94017358f41e0f8133b6
truAgain69 <montaa32@gmail.com>
2022-05-31 10:57:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
